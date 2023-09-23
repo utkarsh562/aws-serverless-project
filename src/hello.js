ce52729da6fea30983570293282d9e66b4384333
Utkarsh Pathak <115210677+utkarsh562@users.noreply.github.com>
2023-09-24 02:16:54 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Batch 4!",
+        message: "Hello From Batch 4!!!",
       },
       null,
       2
